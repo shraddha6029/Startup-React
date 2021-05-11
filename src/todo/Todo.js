cfba4f547d99004e53bdb184e8da523e44c6dfad
Shraddha Chhatraliya <82748995+shraddha6029@users.noreply.github.com>
2021-05-11 11:03:36 +0530

    Todo

--- src/todo/Todo.js
+++ src/todo/Todo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Row,
   Col,
@@ -13,6 +13,20 @@ import {
 } from "reactstrap";
 
 const Todo = () => {
+  const [inputList, setInputList] = useState("Buy Fruits");
+
+  const [items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const addNewList = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+  };
+
   return (
     <>
       <Row>
@@ -21,15 +35,21 @@ const Todo = () => {
             <CardBody>
               <CardTitle tag="h5">Todo List</CardTitle>
               <FormGroup>
-                <Input type="text" name="todo" placeholder="Add Task here" />
+                <Input
+                  type="text"
+                  placeholder="Add Task here"
+                  onChange={itemEvent}
+                />
               </FormGroup>
-              <Button color="primary" className="mt-2">
+              <Button color="primary" className="mt-2" onClick={addNewList}>
                 Add
               </Button>
 
               {/* TASKS */}
               <List className="mt-4">
-                <ListInlineItem>Buy Pen</ListInlineItem>
+                {items.map((val, index) => {
+                  return <ListInlineItem key="index">{val}</ListInlineItem>;
+                })}
               </List>
             </CardBody>
           </Card>
